6b024fabc9ee91f1ff8aeed2de6714ee14ad140b
Yuga Umemura <yugaumemura@YuganoMacBook-Air.local>
2024-12-15 22:24:44 +0900

    first commit

---
+++ build.sbt
@@ -0,0 +1,3 @@
+name := "fpinscala"
+version := "1.0"
+scalaVersion := "3.6.2"
---
+++ src/main/scala/fpinscala/introduction/Introduction.scala
@@ -0,0 +1,24 @@
+package fpinscala.introduction
+
+object IntroductionModule:
+  class Cafe:
+    def buyCoffee(cc: CreditCard): (Coffee, Charge) = {
+      val cup = new Coffee()
+      (cup, Charge(cc, cup.price))
+    }
+
+    def buyCoffees(cc: CreditCard, n: Int): (List[Coffee], Charge) = {
+      val purchases: List[(Coffee, Charge)] = List.fill(n)(buyCoffee(cc))
+      val (coffees, charges) = purchases.unzip
+      (coffees, charges.reduce((c1, c2) => c1.combine(c2)))
+    }
+
+  case class Charge(cc: CreditCard, amount: Double):
+    def combine(other: Charge): Charge =
+      if (cc == other.cc) then Charge(cc, amount + other.amount)
+      else throw new Exception("Can't combine charges to differnt cards")
+
+  class CreditCard
+
+  class Coffee:
+    val price: Double = 2.0
